f96934c1779f05d3569fa9f749732822de59efaf
Alina Bodnar <bodnar.pn@ucu.edu.ua>
2026-04-28 19:52:48 +0300

    small fixes

--- ease-up/src/pages/PublicationsPage.jsx
+++ ease-up/src/pages/PublicationsPage.jsx
@@ -1,35 +1,23 @@
-const publications = [
-  {
-    id: 1,
-    author: 'Karina',
-    date: '20.11.24',
-    title: 'Музей науки у Львові',
-    text: 'Відвідала Музей Науки у Львові. Щира рекомендація кожному бувати в цьому класному доступному місці!',
-    avatar: '/images/avatar.svg',
-    image: '/images/post-image.svg',
-    likes: 45,
-    comments: 3,
-    shares: 1,
-  },
-  {
-    id: 2,
-    author: 'Sophia',
-    date: '20.11.24',
-    title: 'Цікава виставка в ДЗИҐА',
-    text: 'В артцентрі ДЗИҐА на Вірменській зараз проходить цікава виставка. Вхід безкоштовний.',
-    avatar: '/images/avatar.svg',
-    image: '/images/post-image.svg',
-    likes: 68,
-    comments: 7,
-    shares: 3,
-  },
-];
+import InnerPageHeader from '../components/layout/InnerPageHeader';
+import PublicationCard from '../components/cards/PublicationCard';
+import useFetch from '../hooks/useFetch';
+import { getPublications } from '../services/publicationsApi';
+
 export default function PublicationsPage() {
+  const { data: publications, loading, error } = useFetch(getPublications, []);
+
   return (
     <main className="main-content">
-      {publications.map(p => (
-        <div key={p.id}>{p.title}</div>
-      ))}
+      <InnerPageHeader title="Публікації" />
+
+      {loading && <p>Loading...</p>}
+      {error && <p>{error}</p>}
+
+      <section className="page-list">
+        {publications?.map((publication) => (
+          <PublicationCard key={publication.id} {...publication} />
+        ))}
+      </section>
     </main>
   );
 }
